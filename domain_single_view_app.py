44792033d67577df0ef2335226f57064b87156c2
Peter Chaplin <peterchaplin@btinternet.com>
2023-09-09 11:24:32 +0100

    fix2

--- domain_single_view_app.py
+++ domain_single_view_app.py
@@ -15,7 +15,7 @@ from googletrans import Translator
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -27,7 +27,7 @@ headers = {
 @st.cache_resource
 def get_driver():
     return webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()), options=options
+        webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     )
 
 
